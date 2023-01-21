588d6fcf8b50233bb43a406548249b42fa57ea5b
Yuki Yoshimine <yuki07yuki@gmail.com>
2023-01-22 00:54:46 +0800

    feat: run git command

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::process::Command;
+
 fn main() {
-    println!("Hello, world!");
+    // run git command
+    let output = Command::new("git")
+        .arg("status")
+        .output()
+        .expect("failed to execute process");
+
+    // print output
+    println!("{}", String::from_utf8_lossy(&output.stdout));
 }
