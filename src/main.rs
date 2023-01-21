60933317397966a455759dd9bd5637373496a29a
Yuki Yoshimine <yuki07yuki@gmail.com>
2023-01-22 01:36:32 +0800

    feat: get git branch name

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,28 @@
 use std::process::Command;
 
 fn main() {
+    // git branch name
+    let output = Command::new("git")
+        .arg("rev-parse")
+        .arg("--abbrev-ref")
+        .arg("HEAD")
+        .output()
+        .expect("failed to execute process");
+
+    let branch = std::str::from_utf8(&output.stdout).unwrap().trim();
+    println!("{}", branch);
+
     // git log --reverse --pretty=%H master
     let output = Command::new("git")
         .arg("log")
         .arg("--reverse")
         .arg("--pretty=%H")
-        .arg("master")
+        .arg(branch)
         .output()
         .expect("failed to execute process");
 
+    let commits = std::str::from_utf8(&output.stdout).unwrap().trim();
+
     // print output
-    println!("{}", String::from_utf8_lossy(&output.stdout));
+    println!("{}", commits);
 }
