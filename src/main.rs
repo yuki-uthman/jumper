86e1d53e58006403e33afd8b6641c0df0fbf730a
Yuki Yoshimine <yuki07yuki@gmail.com>
2023-01-25 00:12:10 +0800

    refactor: use one function for file change jump

--- src/main.rs
+++ src/main.rs
@@ -199,6 +199,58 @@ fn jump_to_next_change(branch: &str, file: &str) -> Result<(), Error> {
     }
 }
 
+#[derive(PartialEq)]
+enum Direction {
+    Forward,
+    Backward,
+}
+
+fn jump_to_change(direction: Direction, branch: &str, file: &str) -> Result<(), Error> {
+
+    let mut master_log = get_log(branch)?;
+    if direction == Direction::Backward {
+        master_log.reverse();
+    }
+    let head = get_head();
+    let index = master_log.iter().position(|r| r == &head).unwrap();
+
+    let change_log = get_change_log(branch, file);
+
+    // find the next commit that changed the file
+    let next_commit = master_log.iter().skip(index + 1).find(|master_commit| {
+        // skip if the commit is not in the change log
+        if !change_log.contains(master_commit) {
+            false
+        } else {
+            let found = change_log.iter().find(|change_commit| {
+                change_commit == master_commit
+            });
+            match found {
+                Some(_) => true,
+                None => false,
+            }
+        }
+    });
+
+    // checkout next commit
+    match next_commit {
+        Some(commit) => {
+            let output = Command::new("git")
+                .arg("checkout")
+                .arg(commit)
+                .output()
+                .expect("failed to checkout the next commit");
+
+            let stderr = String::from_utf8(output.stderr).unwrap().trim().to_string();
+            println!("{}", stderr);
+            Ok(())
+        }
+        None => {
+            Err(Error::NoMoreChanges(file.to_string()))
+        }
+    }
+}
+
 #[derive(Parser)]
 struct Cli {
     #[arg(short, long, default_value = "master")]
@@ -225,7 +277,7 @@ fn main() {
     match &cli.command {
         Commands::Next { path } => match path {
             Some(path) => {
-                match jump_to_next_change(&branch, path) {
+                match jump_to_change(Direction::Forward, &branch, path) {
                     Ok(_) => {}
                     Err(e) => {
                         println!("{}", e);
@@ -243,7 +295,12 @@ fn main() {
         },
         Commands::Prev { path } => match path {
             Some(path) => {
-                jump_to_prev_change(&branch, path);
+                match jump_to_change(Direction::Backward, &branch, path) {
+                    Ok(_) => {}
+                    Err(e) => {
+                        println!("{}", e);
+                    }
+                }
             }
             None => {
                 jump_to_previous_commit(&branch);
